8b63777d02b742158e1723e9406336d7bba841a4
Bernard065 <bebenibernard@gmail.com>
2024-10-21 00:45:22 +0300

    Fixing deployment errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreBuildErrors: true,
-  },
-};
+const nextConfig = {};
 
 export default withSentryConfig(nextConfig, {
   // For all available options, see:
